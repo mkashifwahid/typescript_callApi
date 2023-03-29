a98fc5c3359eeab7df270fe840a360d916f2d1e9
Muhammad Kashif <mkashif.aw@gmail.com>
2023-03-29 14:12:39 +0500

    Inital commit

---
+++ dist/index.js
@@ -0,0 +1,46 @@
+"use strict";
+// const data = fetch('https://dummyjson.com/products')
+//   .then((res) => res.json())
+//   .then();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const button = document.getElementById('btn');
+const api = 'https://dummyjson.com/products';
+button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
+    fetchData(api);
+});
+function fetchData(api) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log('1');
+        const response = yield fetch(api);
+        const data = yield response.json();
+        show(data);
+    });
+}
+function show(data) {
+    let tab = `<tr>
+          <th>Name</th>
+          <th>Office</th>
+          <th>Position</th>
+          <th>Salary</th>
+         </tr>`;
+    // Loop to access all rows
+    for (let r of data.list) {
+        tab += `<tr>
+    <td>${r.name} </td>
+    <td>${r.office}</td>
+    <td>${r.position}</td>
+    <td>${r.salary}</td>
+</tr>`;
+    }
+    // Setting innerHTML as tab variable
+    document.getElementById('apiResponse').innerHTML = tab;
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+// const data = fetch('https://dummyjson.com/products')
+//   .then((res) => res.json())
+//   .then();
+
+const button = document.getElementById('btn');
+const api: string = 'https://dummyjson.com/products';
+type Product = {
+  id: number;
+  title: string;
+  description: string;
+  brand: string;
+  category: string;
+};
+
+button?.addEventListener('click', function () {
+  fetchData(api);
+});
+
+async function fetchData(api: string) {
+  console.log('1');
+  const response = await fetch(api);
+  const data: Product[] = await response.json();
+  show(data);
+}
+
+function show(data: Product[]) {
+  let tab = `<tr>
+          <th>Name</th>
+          <th>Office</th>
+          <th>Position</th>
+          <th>Salary</th>
+         </tr>`;
+
+  // Loop to access all rows
+  for (let r of data.list) {
+    tab += `<tr>
+    <td>${r.name} </td>
+    <td>${r.office}</td>
+    <td>${r.position}</td>
+    <td>${r.salary}</td>
+</tr>`;
+  }
+  // Setting innerHTML as tab variable
+  document.getElementById('apiResponse').innerHTML = tab;
+}
